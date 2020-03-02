487213371133e7289905cb307512bb15848dd8db
marco2207 <drake2207@gmail.com>
2020-03-02 10:54:34 +0100

    Add files via upload

---
+++ force-app/main/default/lwc/mCDemoLWC/mCDemoLWC.js
@@ -0,0 +1,40 @@
+import { LightningElement, track} from 'lwc';
+import { publish,createMessageContext, APPLICATION_SCOPE, releaseMessageContext, subscribe, unsubscribe } from 'lightning/messageService';
+import lmsDemoMC from "@salesforce/messageChannel/LMSDemoWin__c";
+export default class LMS_MessageSenderReceiverLWC extends LightningElement {
+    @track _msg = '';
+    @track receivedMessage = '';
+    channel;
+    context = createMessageContext();
+   
+    handleSubscribe() {
+        const parentPage = this;
+        this.channel = subscribe(this.context, lmsDemoMC, function (event){
+            if (event != null) {
+                const message = event.messageBody;
+                const source = event.source;
+                parentPage.receivedMessage = 'Message: ' + message + '. Sent From: ' + source;
+            }
+        }, {scope: APPLICATION_SCOPE});
+    }
+
+    handleUnsubscribe() {
+        unsubscribe(this.channel);
+    }
+
+    handleChange(event) { 
+        this._msg = event.target.value;
+    }
+
+    handleClick() {  
+        const payload = {
+            source: "Lightning Web Component",
+            messageBody: this._msg
+        }; 
+        publish(this.context, lmsDemoMC, payload);
+    } 
+
+    disconnectedCallback() {
+        releaseMessageContext(this.context);
+    }
+}
